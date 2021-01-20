9d09581bf8b652af94d64607a979c8e7aa7f8bcd
Birkan Özdemir <birkan_ozdemir@hotmail.com>
2021-01-20 20:41:14 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " eklendi");
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " silindi");
+        }
+        public void ListCustomer(Customer[] customers)
+        {
+            Console.WriteLine("***MÜŞTERİ LİSTESİ***");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Müşteri ID: " + customer.Id);
+                Console.WriteLine("Müşteri Adı: " + customer.FirstName);
+                Console.WriteLine("Müşteri soyadı: " + customer.LastName);
+                Console.WriteLine("Müşteri Kart No: " + customer.CardNumber);
+                Console.WriteLine("**********");
+            }
+            Console.WriteLine("***LİSTE SONU***");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 0;
+            customer1.FirstName = "Ahmet";
+            customer1.LastName = "Aydın";
+            customer1.CardNumber = "1111";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 1;
+            customer2.FirstName = "Fevzi";
+            customer2.LastName = "Küçük";
+            customer2.CardNumber = "2222";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 2;
+            customer3.FirstName = "Mustafa";
+            customer3.LastName = "Korkmaz";
+            customer3.CardNumber = "3333";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 3;
+            customer4.FirstName = "Mehmet";
+            customer4.LastName = "Yalçın";
+            customer4.CardNumber = "4444";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+            customerManager.AddCustomer(customer3);
+            customerManager.AddCustomer(customer4);
+            customerManager.ListCustomer(customers);
+            customerManager.DeleteCustomer(customer3);
+        }
+    }
+}
+
